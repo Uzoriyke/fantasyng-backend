8f25228b2018cd06ad53b8f363946e9cae959893
Uzoriyke <125899393+Uzoriyke@users.noreply.github.com>
2026-04-15 15:20:39 +0100

    Add controllers/userController.js - Profile & User list
    User controller for getting/updating profile and fetching all users. Admin route included.

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.avatar = req.body.avatar || user.avatar;
+
+      const updatedUser = await user.save();
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        avatar: updatedUser.avatar
+      });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).select('-password');
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
