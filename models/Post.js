39b643c4026ad222f32614e943cace6a0001e1e3
Uzoriyke <125899393+Uzoriyke@users.noreply.github.com>
2026-04-15 15:02:20 +0100

    Re-add models/Post.js
    Restore Post model. Handles user posts with likes and comments.

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+  isReported: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Post', postSchema);
